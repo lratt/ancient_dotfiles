caf8455a6b398d413a08625b0919894a9506ad49
lratt <lratt@mailbox.org>
2022-07-24 23:37:27 +0200

    use cargo install instead of git clone

--- install.sh
+++ install.sh
@@ -15,8 +15,7 @@ cargo_init() {
   done
 
   # install nvim package manager
-  rm -rf /tmp/npk
-  git clone https://github.com/lratt/npk.git /tmp/npk && cargo install --path /tmp/npk
+  cargo install --git https://github.com/lratt/npk npk
 }
 
 tpm_init() {
